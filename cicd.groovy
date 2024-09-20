eca9ee5c2f440c3d537f5dd97aef47ef38638b81
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:40 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gawkport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/gawkport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gawkport.git'), string(name: 'PORT_DESCRIPTION', value: 'The gawk utility interprets a special-purpose programming language that makes it possible to handle simple data-reformatting jobs with just a few lines of code.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/gawkport.git'), string(name: 'PORT_DESCRIPTION', value: 'The gawk utility interprets a special-purpose programming language that makes it possible to handle simple data-reformatting jobs with just a few lines of code.' )]
         }
 }
